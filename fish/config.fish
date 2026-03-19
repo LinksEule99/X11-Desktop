3474f0bca4d9208ffa14573009b356edbd6ccc00
Commiemo <140261372+LinksEule99@users.noreply.github.com>
2026-03-19 18:08:13 +0100

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -g fish_greeting
+set -gx PATH /usr/local/bin $PATH
+set -gx PATH /home/henry/.local/bin $PATH
